323c33af140f381915a5d2d7bcdb77c4b5c81b98
David Thorn <david.thorn221278@googlemail.com>
2026-01-30 12:01:20 +0100

    Initial Commit

---
+++ MyHealth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyHealth
+//
+//  Created by David Thorn on 30.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyHealth/MyHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHealthApp.swift
+//  MyHealth
+//
+//  Created by David Thorn on 30.01.26.
+//
+
+import SwiftUI
+
+@main
+struct MyHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
